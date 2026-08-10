bc014d0a48e2509ea15b95d8d0b3b2ea336d0dc4
PicksN1k <csjohnson2838@gmail.com>
2026-08-10 16:30:13 -0400

    modified:   2_React/2_react/src/Login.js

--- 2_React/2_react/src/Login.js
+++ 2_React/2_react/src/Login.js
@@ -1,4 +1,94 @@
-</form>
+import { useState } from "react";
+
+
+export default function Login({ onLogin }) {
+
+  const [username, setUsername] =
+    useState("");
+
+  const [password, setPassword] =
+    useState("");
+
+  const [error, setError] =
+    useState("");
+
+
+  async function handleSubmit(event) {
+
+    event.preventDefault();
+
+    try {
+
+      setError("");
+
+      await onLogin(
+        username,
+        password
+      );
+
+    } catch (error) {
+
+      console.error(error);
+
+      setError(
+        "Invalid username or password"
+      );
+
+    }
+  }
+
+
+  return (
+
+    <div>
+
+      <h1>CS602 Shopping Cart</h1>
+
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        <div>
+
+          <label>Username: </label>
+
+          <input
+            type="text"
+            value={username}
+            onChange={
+              (e) =>
+                setUsername(e.target.value)
+            }
+            required
+          />
+
+        </div>
+
+        <br />
+
+        <div>
+
+          <label>Password: </label>
+
+          <input
+            type="password"
+            value={password}
+            onChange={
+              (e) =>
+                setPassword(e.target.value)
+            }
+            required
+          />
+
+        </div>
+
+        <br />
+
+        <button type="submit">
+          Login
+        </button>
+
+      </form>
 
       {error && (
         <p>{error}</p>
